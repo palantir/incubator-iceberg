c7fd73ec6b1fde8b9c6dc283d76f708c12dbde36
Yifei Huang <yifeih@palantir.com>
2019-04-15 17:52:57 -0700

    Upgrade guava to 23.6.1-jre (#8)
    * upgrade guava to palantir's spark version
    
    * dont hive import the world
    
    * dont wild card import

--- common/src/main/java/com/netflix/iceberg/common/DynFields.java
+++ common/src/main/java/com/netflix/iceberg/common/DynFields.java
@@ -20,7 +20,7 @@
 package com.netflix.iceberg.common;
 
 import com.google.common.base.Joiner;
-import com.google.common.base.Objects;
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
 import com.google.common.collect.Sets;
@@ -66,7 +66,7 @@ public class DynFields {
     }
 
     public String toString() {
-      return Objects.toStringHelper(this)
+      return MoreObjects.toStringHelper(this)
           .add("class", field.getDeclaringClass().toString())
           .add("name", name)
           .add("type", field.getType())
